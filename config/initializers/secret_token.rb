118c62e1a53142ad05e23e63db09dbc1cd49c8e1
claudiu <claudiu.garba@gmail.com>
2013-10-18 23:31:42 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TempModelQuery::Application.config.secret_key_base = '0dae6aa69ca857238d16dd1e33bab418c43cc9a1e58fe551fbb60b8c43947bc585ad9b49ec53d8ed4100874e97cec729e8d548466a5fe0dd6f5ae82ee21fadb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TempModelQuery::Application.config.session_store :cookie_store, key: '_temp_model_query_session'
